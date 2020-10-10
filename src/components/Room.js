7839bf62ff7b4ef4f341f107eed7d362faf8b437
matthias haefeli <haefeli.matthias@gmail.com>
2020-10-10 11:58:48 -0600

    prop types

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import defaultImg from '../images/room-1.jpeg'
+import PropTypes from 'prop-types'
 
 export default function Room({ room }) {
   const { name, slug, images, price } = room;
@@ -15,6 +16,16 @@ export default function Room({ room }) {
         </div>
         <Link to={`/rooms/${slug}`} className='btn-primary room-link'>Features</Link>
       </div>
+      <p className='room-info'>{name}</p>
     </article>
   )
+}
+
+Room.propTypes = {
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string),
+    price: PropTypes.number.isRequired
+  })
 }
