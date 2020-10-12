3d790cc0a38ce843f5dbde2f9695b007ae66f7eb
matthias haefeli <haefeli.matthias@gmail.com>
2020-10-12 09:29:42 -0600

    size, breakfast and pets filter

--- src/context.js
+++ src/context.js
@@ -54,7 +54,7 @@ class RoomProvider extends Component {
 
   handleChange = event => {
     const target = event.target
-    const value = event.type === 'checkbox' ? target.checked : target.value
+    const value = target.type === 'checkbox' ? target.checked : target.value
     const name = event.target.name
     this.setState({
       [name]: value
@@ -83,6 +83,19 @@ class RoomProvider extends Component {
 
     // filter by price
     tempRooms = tempRooms.filter(room => room.price <= price)
+
+    // filter by size
+    tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+
+    // filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true)
+    }
+
+    // fitler by pets
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true)
+    }
 
     this.setState({
       sortedRooms: tempRooms
